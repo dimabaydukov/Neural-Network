03d26c23fc43297cbbd35b255b603223e5e1ffc3
dimabaydukov <18052002@bk.ru>
2022-09-30 23:08:31 +0300

    Update

--- Neural Network.py
+++ Neural Network.py
@@ -1,41 +1,21 @@
-import os
-import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
+import pickle
 
-"""
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+with open("Data set/pickled_mnist.pkl", "br") as fh:
+    data = pickle.load(fh)
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
+image_size = 28
+labels = 10
+image_pixels = image_size * image_size
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+lr = np.arange(labels)
 
-model.fit(x_train, y_train, epochs=50)
+train_images = data[0]
+test_images = data[1]
+train_labels = data[2]
+test_labels = data[3]
 
-model.save('neural_network.model')
-"""
-
-model = tf.keras.models.load_model('neural_network.model')
-
-image_number = 1
-while os.path.isfile(f"Digits/{image_number}.png"):
-    try:
-        img = cv2.imread(f"Digits/{image_number}.png")[:, :, 0]
-        img = np.invert(np.array([img]))
-        prediction = model.predict(img)
-        print(f"The number is {np.argmax(prediction)}")
-        plt.imshow(img[0], cmap=plt.cm.binary)
-        plt.show()
-    except ImportError:
-        print("Error")
-    finally:
-        image_number += 1
+train_labels_one_hot = (lr == train_labels).astype(np.float64)
+test_labels_one_hot = (lr == test_labels).astype(np.float64)
